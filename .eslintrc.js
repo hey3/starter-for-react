09718665f288d324bace7bb2e9f396639d937932
hey3 <oh1kohei@gmail.com>
2020-04-09 00:08:43 +0900

    fix to use eslint config from npm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  extends: ['./eslintConfig/eslint.js', './eslintConfig/typescript.js', './eslintConfig/react.js'],
+  extends: ['@hey3/eslint-config'],
   settings: {
     'import/resolver': {
       node: {
@@ -9,19 +9,5 @@ module.exports = {
       },
       webpack: { config: path.join(__dirname, './webpack.config.js') },
     },
-  },
-  rules: {
-    'prettier/prettier': ['error'],
-    '@typescript-eslint/no-explicit-any': 'off',
-    'import/no-unresolved': 'off',
-    'jsx-a11y/label-has-for': 'off',
-    'no-restricted-syntax': [
-      'error',
-      {
-        selector: 'TSEnumDeclaration',
-        message: "Don't declare enums",
-      },
-    ],
-    'no-use-before-define': 'off',
   },
 }
