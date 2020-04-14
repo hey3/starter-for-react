c6ef495cf69935738be2d4a30e48d20e46042a29
Kohei Oyama <kohei.oyama@access-company.com>
2020-04-14 18:45:12 +0900

    fix to be able to use alias and image in storybook (#12)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,34 @@
+const path = require('path')
+
 module.exports = {
   resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@pages': path.resolve(__dirname, '../src/pages'),
+      '@images': path.resolve(__dirname, '../static/images'),
+    },
     extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+        exclude: /node_modules/,
       },
     ],
   },
