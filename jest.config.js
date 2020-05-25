4f1a43eda3e4d49c50bc731200ce3ec8cb8dd2be
Kohei Oyama <kohei.oyama@access-company.com>
2020-05-25 14:57:22 +0900

    fix jest config (#83)

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: 'coverage',
   moduleNameMapper: {
+    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
     '^@(components|pages|themes)/(.+)': '<rootDir>/src/$1/$2',
-    '^@images/(.+)': '<orotDr>/static/images/$1',
   },
   roots: ['<rootDir>/src'],
   setupFiles: ['<rootDir>/src/enzymeConfig.ts'],
