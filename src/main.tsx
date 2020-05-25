2504d1b2eecd8077cbf71b2a16d4595dc22517b7
hey3 <oh1kohei@gmail.com>
2020-05-25 09:59:18 +0900

    remove styled reset (#82)
    * Remove styled-reset
    * Remove html:62.5% config to use ps2rem

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { createGlobalStyle } from 'styled-components'
-import reset from 'styled-reset'
 
 import { ThemeProvider } from '@themes/ThemeProvider'
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}
-  html {
-    font-size: 62.5%;
-  }
   body {
     word-wrap: break-word;
     font-family: '游ゴシック体', YuGothic, '游ゴシック', 'Yu Gothic', 'メイリオ', sans-serif;
