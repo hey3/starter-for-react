279f00fedf770be823c0bd59f218c066d8ee5549
Kohei Oyama <kohei.oyama@access-company.com>
2020-09-30 19:00:44 +0900

    update storybook config (#135)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,11 @@
 const custom = require('../webpack.config.js')
 
 module.exports = {
-  addons: [
-    '@storybook/addon-a11y',
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs',
-    '@storybook/addon-viewport',
-    'storybook-readme',
-  ],
+  addons: ['@storybook/addon-a11y', '@storybook/addon-essentials', '@storybook/addon-knobs'],
   stories: ['../src/components/**/*.stories.tsx'],
-  webpackFinal: (config) => {
-    return {
-      ...config,
-      module: { ...config.module, rules: custom.module.rules },
-      resolve: custom.resolve,
-    }
-  },
+  webpackFinal: config => ({
+    ...config,
+    module: { ...config.module, rules: custom.module.rules },
+    resolve: custom.resolve,
+  }),
 }
