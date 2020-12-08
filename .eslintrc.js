269bbdf89d793c2d838c9be9d9f282964be925c0
Kohei Oyama <kohei.oyama@access-company.com>
2020-12-08 13:07:54 +0900

    update packages (#162)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,58 @@
 const path = require('path')
 
 module.exports = {
-  extends: ['@hey3/eslint-config'],
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc'],
+  extends: ['eslint:recommended'],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      settings: { react: { version: 'detect' } },
+      env: {
+        browser: true,
+        es2021: true,
+        node: true,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+        'plugin:jsx-a11y/recommended',
+        'prettier/@typescript-eslint',
+        'prettier/react',
+      ],
+      rules: {
+        'prettier/prettier': [
+          'error',
+          {},
+          {
+            usePrettierrc: true,
+          },
+        ],
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/explicit-function-return-type': [
+          'warn',
+          {
+            allowExpressions: true,
+            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+          },
+        ],
+      },
+    },
+  ],
   settings: {
     'import/resolver': {
       node: {
