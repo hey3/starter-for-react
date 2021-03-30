291fccfba7e66a1c15bd2ad07f1eee28cd7938dc
hey3 <oh1kohei@gmail.com>
2021-03-30 13:50:27 +0900

    Update dependencies (#191)

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,39 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
-const webpack = require('webpack')
 const Dotenv = require('dotenv-webpack')
-const env = process.env.BUILD_MODE
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const defineEnv = new webpack.DefinePlugin({
-  'process.env': {
-    REACT_APP_ENV: JSON.stringify(process.env.REACT_APP_ENV),
-  },
-})
+const mode = process.env.BUILD_MODE || 'development'
+const env = process.env.REACT_APP_ENV || 'local'
 
 module.exports = {
-  mode: env || 'development',
-  entry: './src/main.tsx',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/',
-    filename: 'build.js',
+  mode,
+  cache: {
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename],
+    },
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
+    // hot: true,
+    // hotOnly: true,
+    open: true,
+  },
+  devtool: env === 'production' ? false : 'inline-source-map',
+  entry: {
+    main: {
+      import: ['./src/main.tsx'],
+    },
+  },
+  experiments: {},
+  // FIXME: assetModulesを使うように修正
+  // experiments: {
+  //   asset: true,
+  // },
+  externals: {},
+  externalsPresets: {},
+  infrastructureLogging: {},
   module: {
     rules: [
       {
@@ -29,8 +45,30 @@ module.exports = {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
         loader: 'file-loader',
       },
+      // FIXME: assetModulesを使うように修正
+      // {
+      //   test: /\.(png|jpg|gif)$/,
+      //   type: 'asset',
+      // },
     ],
   },
+  node: {},
+  optimization: {},
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'build.js',
+    assetModuleFilename: 'images/[hash][ext]',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new Dotenv({
+      path: `./src/envs/.env.${env}`,
+    }),
+  ],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
@@ -40,19 +78,9 @@ module.exports = {
     },
     extensions: ['.js', '.ts', '.tsx'],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    defineEnv,
-    new Dotenv({
-      path: `./src/envs/.env.${process.env.REACT_APP_ENV}`,
-    }),
-  ],
-  devtool: env === 'production' ? false : 'inline-source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    historyApiFallback: true,
-  },
+  resolveLoader: {},
+  snapshot: {},
+  stats: {},
+  target: 'web',
+  watchOptions: {},
 }
