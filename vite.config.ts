6a919f726dd1ffce6c89fc514a99b7f126cdf901
hey3 <oh1kohei@gmail.com>
2022-04-01 02:21:26 +0900

    add vite config

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import reactPlugin from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [reactPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'test/setup.ts',
+  },
+})
