55a8f35b234df754994db863a48faee8a8d7c384
hey3 <oh1kohei@gmail.com>
2022-05-10 00:51:41 +0900

    IS_REACT_ACT_ENVIRONMENT is no longer needed with the vitest update

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,1 @@
 import '@testing-library/jest-dom'
-
-// @see https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#configuring-your-testing-environment
-const globalThisForTest = global as typeof globalThis & {
-  IS_REACT_ACT_ENVIRONMENT: boolean
-}
-globalThisForTest.IS_REACT_ACT_ENVIRONMENT = true
