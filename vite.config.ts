cb970342a975adb22f81ea52db98d7401fd319ca
hey3 <oh1kohei@gmail.com>
2022-05-21 23:22:11 +0900

    migrate vitest 0.12.8

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'test/setup.ts',
+    setupFiles: ['./test/setup.ts'],
+    passWithNoTests: true,
   },
 })
