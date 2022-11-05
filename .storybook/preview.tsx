9c8d2f36cbb5f058f2f76c548148390fc4a1c594
hey3 <oh1kohei@gmail.com>
2022-11-05 18:23:32 +0900

    feat: update storybook config for css modules

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,19 @@
 import type { DecoratorFunction } from '@storybook/addons'
 import React from 'react'
+import { ThemeProvider } from '../src/stores/ThemeStore'
+import '../src/styles/reset.css'
+import '../src/styles/global.css'
 
-export const decorators: DecoratorFunction<JSX.Element>[] = [Story => <Story />]
+export const decorators: DecoratorFunction<JSX.Element>[] = [
+  Story => {
+    // Reset theme for each story.
+    window.document.documentElement.setAttribute('data-theme', 'light')
+    window.localStorage.clear()
+
+    return (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    )
+  },
+]
