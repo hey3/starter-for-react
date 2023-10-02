d8280756679f95e1299af29ceaae4499022c5d65
hey3 <oh1kohei@gmail.com>
2023-10-02 21:34:43 +0900

    refactor: replace to use named export

--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -15,5 +15,5 @@ const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>
   render(ui, { wrapper: AllTheProviders, ...options })
 
 export * from '@testing-library/react'
-export { default as userEvent } from '@testing-library/user-event'
+export { userEvent } from '@testing-library/user-event'
 export { customRender as render }
