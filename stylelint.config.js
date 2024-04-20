04ddf6288786dd9cdd325da9f6119a3e8bfeefae
hey3 <oh1kohei@gmail.com>
2024-04-20 15:26:14 +0900

    feat: migrate stylelint config to esm

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('stylelint').Config} */
+export default {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   ignoreFiles: ['src/styles/reset.css'],
   rules: {
