e3a720a1e461b1cb16f2b57de1416a6dd546bcdb
hey3 <oh1kohei@gmail.com>
2024-04-23 23:29:26 +0900

    feat: change directory structure

--- src/app.tsx
+++
@@ -1,8 +0,0 @@
-import { type FC } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-
-export const App: FC = () => (
-  <BrowserRouter>
-    <div>Hellow World!</div>
-  </BrowserRouter>
-)
